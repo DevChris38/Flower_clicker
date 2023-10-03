5f9cc298338d9596ae31535298944f6b15014114
Christopher Arthaud <christopher.arthaud@gmail.com>
2023-10-03 23:29:39 +0200

    Revert "bug buying multiple arrosoir fixed"
    This reverts commit f61cb0fb194dd07402c2d06bea5b2b848a473e2b.

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let fini = false;
 let tempsPrecedent = 0;
 let lastClick = 0;
 let precedentTimeStamp = 0;
-let precedentTimeStampArrosoir = 0;
 
 
 // tableau d'objets pour définir les fleurs que l'on va gagner
@@ -97,11 +96,11 @@ function updateScore(chrono) {
     //Quand on a assez d'argent pour acheter l'arrosoir, permet de l'acheter et d'augmenter le gain de pétales
     arrosoir.addEventListener("click", (event) => {
         event.preventDefault();
-        if (actScore >= helpersProps[0].cost && event.timeStamp != precedentTimeStampArrosoir) {
+        if (actScore >= helpersProps[0].cost && lastClick < (chrono - 500)) {
             actScore -= helpersProps[0].cost;
             helpersProps[0].number++;
             petalesPerSeconds++;
-            precedentTimeStampArrosoir = event.timeStamp;
+            lastClick = chrono;
         }
         ;
     }
