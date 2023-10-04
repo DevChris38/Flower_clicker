9080b75f7436a0a9d432c32f6edbb30b16e70375
DevChris38 <christopher.arthaud@gmail.com>
2023-10-04 15:16:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let fini = false;
 let tempsPrecedent = 0;
 let lastClick = 0;
 let precedentTimeStamp = 0;
+let precedentTimeStampArrosoir = 0;
 
 
 
@@ -97,11 +98,11 @@ function updateScore(chrono) {
     //Quand on a assez d'argent pour acheter l'arrosoir, permet de l'acheter et d'augmenter le gain de pétales
     arrosoir.addEventListener("click", (event) => {
         event.preventDefault();
-        if (actScore >= helpersProps[0].cost && lastClick < (chrono - 500)) {
+        if (actScore >= helpersProps[0].cost && event.timeStamp != precedentTimeStampArrosoir) {
             actScore -= helpersProps[0].cost;
             helpersProps[0].number++;
             petalesPerSeconds++;
-            lastClick = chrono;
+            precedentTimeStampArrosoir = event.timeStamp;
         }
         ;
     }
